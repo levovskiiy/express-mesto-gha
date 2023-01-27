fd764f0d68c3320b992466f8eca71ec6f0549448
lev cherepanov <levovskiiy@yandex.ru>
2023-01-28 00:51:10 +0500

    fix: пофиксил модель карточки

--- models/card.js
+++ models/card.js
@@ -3,7 +3,7 @@ const { Schema, model } = require('mongoose')
 const owner = { type: Schema.Types.ObjectId, ref: 'User', required: true }
 
 const Card = new Schema({
-  name: { type: String, minlength: 2, maxlength: 30, required: true },
+  name: { type: String, minLength: 2, maxLength: 30, required: true },
   link: { type: String, required: true },
   owner: { ...owner },
   likes: [{ ...owner }],
