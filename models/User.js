d16b503390070189c2ed2ab189a6bcdd02b95bb3
lev cherepanov <levovskiiy@yandex.ru>
2023-01-28 00:51:54 +0500

    fix: пофиксил модель юзера

--- models/User.js
+++ models/User.js
@@ -1,8 +1,8 @@
 const { Schema, model } = require('mongoose')
 
 const User = new Schema({
-  name: { type: String, minlength: 2, maxlength: 30, required: true },
-  about: { type: String, minlength: 2, maxlength: 30, required: true },
+  name: { type: String, minLength: 2, maxLength: 30, required: true },
+  about: { type: String, minLength: 2, maxLength: 30, required: true },
   avatar: { type: String, required: true },
 })
 
